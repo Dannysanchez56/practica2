1f7b305a5e3f8ce8a1e890370eee5cbdddd3b69b
Dannysanchez56 <99843423+Dannysanchez56@users.noreply.github.com>
2022-02-16 18:50:42 -0600

    Update init.c

--- init.c
+++ init.c
@@ -4,18 +4,22 @@
 #include <sys/wait.h>
 
 void crear_proceso(){
-    int p = fork();
+    p = fork();
 
     if (p==0){
-        execlp("xterm","xterm","-e", "./getty.out", NULL);
+        execvp("xterm","-e", "./getty.out", NULL);
     }
 }
 
 int main(){
     int i;
+     int p;
     for (i = 0 ; i < 6 ; i++){
         crear_proceso();
     }
-    //if(si uno de los procesos se cierra, correr otro){
+    
+    while(1){   
+        wait(NULL);     //si uno de los procesos se cierra, correr otro
         crear_proceso();
+    }
 }
