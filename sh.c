2615137084d57a1d84b159b882a638b006c90b8c
Dannysanchez56 <99843423+Dannysanchez56@users.noreply.github.com>
2022-02-16 18:50:49 -0600

    Update sh.c

--- sh.c
+++ sh.c
@@ -8,27 +8,26 @@
 //ejemplo de input: ./ejer1.out
 
 int main(){
-    char comando[30];
+   	char comando[30];
 	int pid;
 	int salida = 0;
 	
 	do{
 		printf(">");
-		fgets(comando,  80, stdin);
-		strtok(comando, "\n");
-		if(strcmp(comando, "exit") == 0)
+		fgets(comando, 80, stdin);
+		strtok(comando,"\n");
+		if(strcmp(comando, "exit") == 0){
 			salida = 1;
-        if(strcmp(comando, "shutdown") == 0)
-			execlp("killall5 -9", "killall5 -9" , NULL);
-		else{
-            pid = fork();
-            if(pid == 0){
-                execlp(comando, comando, NULL);
-            }
-            else{
-                wait(NULL);
-                }
-        }         
+		}else if(strcmp(comando, "shutdown") == 0){
+			execlp("killall5 -9", "killall5 -9" ,"init.out","getty.out", "sh.out" NULL);
+		}else{
+            		pid = fork();
+            		if(pid == 0){
+                		execlp(comando, comando, NULL);
+            		}else{
+                		wait(NULL);
+                	}
+        	}
 		memset(comando, 0, sizeof(comando));
     }while(!salida);
 }
